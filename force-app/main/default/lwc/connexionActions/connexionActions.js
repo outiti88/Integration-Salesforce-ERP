439318dc01252f0a3e3b446f0b4f88efb2e99cf7
outiti88 <outiti.ayoub@gmail.com>
2021-04-26 23:05:11 +0000

    Add helper.js

--- force-app/main/default/lwc/connexionActions/connexionActions.js
+++ force-app/main/default/lwc/connexionActions/connexionActions.js
@@ -3,6 +3,7 @@ import getIsBasic from "@salesforce/apex/ConnexionController.getIsBasic";
 import negateActivation from "@salesforce/apex/ConnexionController.negateActivation";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import { getRecordNotifyChange } from "lightning/uiRecordApi";
+import { reduceErrors } from "c/helpers";
 
 export default class ConnexionActions extends LightningElement {
   @api recordId;
@@ -54,47 +55,3 @@ export default class ConnexionActions extends LightningElement {
   }
 }
 
-function reduceErrors(errors) {
-  if (!Array.isArray(errors)) {
-    errors = [errors];
-  }
-
-  return (
-    errors
-      // Remove null/undefined items
-      .filter((error) => !!error)
-      // Extract an error message
-      .map((error) => {
-        // UI API read errors
-        if (Array.isArray(error.body)) {
-          return error.body.map((e) => e.message);
-        }
-        // UI API DML, Apex and network errors
-        else if (error.body && typeof error.body.message === "string") {
-          return error.body.message;
-        }
-        // JS errors
-        else if (typeof error.message === "string") {
-          return error.message;
-        }
-        // Validation Rules Errors
-        else if (error.body.fieldErrors) {
-          const { fieldErrors } = error.body;
-          const errorMessages = [];
-          for (const key in fieldErrors) {
-            errorMessages.push({
-              title: fieldErrors[key][0].statusCode,
-              message: fieldErrors[key][0].message
-            });
-          }
-          return errorMessages;
-        }
-        // Unknown error shape so try HTTP status text
-        return error.statusText;
-      })
-      // Flatten
-      .reduce((prev, curr) => prev.concat(curr), [])
-      // Remove empty strings
-      .filter((message) => !!message)
-  );
-}
